79673f9987ad60974ad41124c0608be9ee9e4f77
Michael Fürst <mail@michaelfuerst.de>
2015-10-28 16:11:17 +0100

    Fully implemented server backend for protected environments. You can requiest graphs and node lists. You can save graphs overwrite enabled.

--- grapheditor/bin/app.rb
+++ grapheditor/bin/app.rb
@@ -8,3 +8,37 @@ set :views, "views"
 get '/' do
     erb :index, :locals => {'params' => params}
 end
+
+get '/load/' do
+	if not params[:graph].nil? then
+		graphfile = "data/" + params[:graph] + ".graph.json"
+		file = File.open( graphfile, "rb")
+		graph = file.read
+    	return graph
+	end
+	if not params[:nodes].nil? then
+		nodefile = "data/" + params[:nodes] + ".nodes.json"
+		file = File.open(nodefile, "rb")
+		nodes = file.read
+    	return nodes
+	end
+	return "Error"
+end
+
+post '/save/' do
+	if not params[:graph].nil? and not params[:data].nil? then
+		graphfile = "data/" + params[:graph] + ".graph.json"
+		File.open(graphfile, 'w') { |file| file.write(params[:data]) }
+    	return "Success"
+	end
+	return "Error"
+end
+
+get '/save/' do
+	if not params[:graph].nil? and not params[:data].nil? then
+		graphfile = "data/" + params[:graph] + ".graph.json"
+		File.open(graphfile, 'w') { |file| file.write(params[:data]) }
+    	return "Success"
+	end
+	return "Error"
+end
