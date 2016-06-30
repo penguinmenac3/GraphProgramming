1c4106fc95976b712b34da1d8e1c5283b6624a46
Michael Fürst <mail@michaelfuerst.de>
2016-06-30 15:32:58 +0200

    Fixed video to work with cv3 and width and height.

--- python/extlib/computervision/video.py
+++ python/extlib/computervision/video.py
@@ -11,7 +11,7 @@ except ValueError:
 class Node(base.Node):
     def __init__(self, verbose, args):
         super(Node, self).__init__("Video", "computervision.video",
-                                   {"resource": 0, "fps": 30},
+                                   {"resource": 0, "fps": 30, "exposure":0.1},
                                    {},
                                    {"result": "Image"},
                                    "Capture video.", verbose, True, True)
@@ -20,14 +20,18 @@ class Node(base.Node):
 
     def set_res(self, x,y):
         pass
-        #self.cap.set(cv.CV_CAP_PROP_FRAME_WIDTH, int(x))
-        #self.cap.set(cv.CV_CAP_PROP_FRAME_HEIGHT, int(y))
-        #return self.cap.get(cv.CV_CAP_PROP_FRAME_WIDTH), self.cap.get(cv.CV_CAP_PROP_FRAME_HEIGHT)
+        self.cap.set(3, int(x))
+        self.cap.set(4, int(y))
+        return self.cap.get(3), self.cap.get(4)
+      
+    def set_exposure(self, exposure):
+        self.cap.set(15, exposure)
 
     def tick(self, value):
         if self.cap is None:
             self.cap = cv2.VideoCapture(self.args["resource"])
-            # self.set_res(1024.0, 768.0)
+            self.set_res(1024.0, 768.0)
+            self.set_exposure(self.args["exposure"])
             #self.set_res(1280,720)
             if not self.cap.isOpened():
                 raise Exception("Cannot open the given resource: ", self.args["resource"])
